d47928da8780fdc62dcacd6678554197e5150770
Lorenzo Gaborini <lorenzo.gaborini@gmail.com>
2020-08-27 23:12:08 +0200

    Dev script

--- dev/dev_package.R
+++ dev/dev_package.R
@@ -8,25 +8,31 @@ library(magrittr)
 
 seed <- sample.int(20, 1)
 
-n <- 10
-m <- 5
-alpha_0 <- 1e1
-beta_0 <- 1e1
 p <- 3
-nu_0 <- rep(1, p)
+conc <- 20
+nu_0 <- c(0.4, 0.6, 0.5)
+nu_0 <- nu_0 / sum(nu_0) * conc
 
 set.seed(seed)
 mtx_beta <- rdirichlet_beta_cpp(1000, nu_0);
 mtx_gamma <- purrr::map(seq(1000), ~ rdirichlet_cpp(nu_0, seed = sample.int(2000, 1))) %>% { do.call(rbind, .) }
 
-# plot(mtx_beta[,1], mtx_beta[,2])
-# points(mtx_gamma[,1], mtx_gamma[,2], col = 'red')
-
+plot(mtx_beta[,1], mtx_beta[,2], xlim = c(0, 1), ylim = c(0, 1))
+points(mtx_gamma[,1], mtx_gamma[,2], col = 'red')
 
 
 # Population --------------------------------------------------------------
 
 
+n <- 100
+m <- 10
+alpha_0 <- 1e1
+beta_0 <- 1e1
+conc <- 10
+nu_0 <- rep(1, p)
+nu_0 <- nu_0 / sum(nu_0) * conc
+
+# set.seed(seed)
 
 mtx_obs <- rdirdirgamma_cpp(
    n = n,
@@ -36,8 +42,25 @@ mtx_obs <- rdirdirgamma_cpp(
    nu_0 = nu_0
 )
 
+# set.seed(seed)
 
-sample_ABC_rdirdirgamma_cpp_internal(
+mtx_obs_beta <- rdirdirgamma_beta_cpp(
+   n = n,
+   m = m,
+   alpha_0 = alpha_0,
+   beta_0 = beta_0,
+   nu_0 = nu_0
+)
+
+plot(mtx_obs[,1], mtx_obs[,2], xlim = c(0, 1), ylim = c(0, 1))
+points(mtx_obs_beta[,1], mtx_obs_beta[,2], col = 'red')
+
+
+# ABC / Rcpp --------------------------------------------------------------
+
+
+
+sample_ABC_rdirdirgamma_cpp_wrapper(
    n = n,
    m = m,
    alpha_0 = alpha_0,
