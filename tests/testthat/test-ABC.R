287685e0cc642e5c9e493233acf76032ed1719e9
Lorenzo Gaborini <lorenzo.gaborini@gmail.com>
2020-09-13 17:25:58 +0200

    More ABC tests

--- tests/testthat/test-ABC.R
+++ tests/testthat/test-ABC.R
@@ -126,3 +126,127 @@ test_that("sample_ABC_rdirdirgamma_beta_cpp works: wrong columns", {
 #    expect_equal(ncol(mtx_dist), 2)
 #    expect_true(all(mtx_dist >= 0))
 # })
+
+
+
+# Full ABC computation ----------------------------------------------------
+
+
+
+test_that("compute_ABC_cpp works: standard, no distances", {
+
+   n_summary <- expect_silent(get_number_summary_statistics(FALSE))
+   vec_eps <- rep(1, n_summary)
+
+   list_ABC <- expect_silent(compute_ABC_cpp(
+         n_sample = 10,
+         m_sample = 10,
+         alpha_0 = 1,
+         beta_0 = 1,
+         nu_0 = rep(1, p),
+         mtx_obs,
+         summarize_eps = vec_eps,
+         reps = 100,
+         p_norm = 2,
+         use_optimized_summary = FALSE,
+         return_distances = FALSE
+   ))
+
+   expect_is(list_ABC, 'list')
+   expect_named(list_ABC, c('n_accepted', 'accept_ratio'))
+   expect_gte(list_ABC$n_accepted, 0)
+   expect_lte(list_ABC$n_accepted, 100)
+   expect_gte(list_ABC$accept_ratio, 0)
+   expect_lte(list_ABC$accept_ratio, 1)
+})
+
+
+test_that("compute_ABC_cpp works: optimized, no distances", {
+
+   n_summary <- expect_silent(get_number_summary_statistics(TRUE))
+   vec_eps <- rep(1, n_summary)
+
+   list_ABC <- expect_silent(compute_ABC_cpp(
+         n_sample = 10,
+         m_sample = 10,
+         alpha_0 = 1,
+         beta_0 = 1,
+         nu_0 = rep(1, p),
+         mtx_obs,
+         summarize_eps = vec_eps,
+         reps = 100,
+         p_norm = 2,
+         use_optimized_summary = TRUE,
+         return_distances = FALSE
+   ))
+
+   expect_is(list_ABC, 'list')
+   expect_named(list_ABC, c('n_accepted', 'accept_ratio'))
+   expect_gte(list_ABC$n_accepted, 0)
+   expect_lte(list_ABC$n_accepted, 100)
+   expect_gte(list_ABC$accept_ratio, 0)
+   expect_lte(list_ABC$accept_ratio, 1)
+})
+
+test_that("compute_ABC_cpp works: standard, with distances", {
+
+   n_summary <- expect_silent(get_number_summary_statistics(TRUE))
+   vec_eps <- rep(1, n_summary)
+
+   list_ABC <- expect_silent(compute_ABC_cpp(
+         n_sample = 10,
+         m_sample = 10,
+         alpha_0 = 1,
+         beta_0 = 1,
+         nu_0 = rep(1, p),
+         mtx_obs,
+         summarize_eps = vec_eps,
+         reps = 100,
+         p_norm = 2,
+         use_optimized_summary = TRUE,
+         return_distances = TRUE
+   ))
+
+   expect_is(list_ABC, 'list')
+   expect_named(list_ABC, c('n_accepted', 'accept_ratio', 'd_ABC'))
+   expect_gte(list_ABC$n_accepted, 0)
+   expect_lte(list_ABC$n_accepted, 100)
+   expect_gte(list_ABC$accept_ratio, 0)
+   expect_lte(list_ABC$accept_ratio, 1)
+   expect_is(list_ABC$d_ABC, 'matrix')
+   expect_equal(nrow(list_ABC$d_ABC), 100)
+   expect_equal(ncol(list_ABC$d_ABC), n_summary)
+   expect_true(all(list_ABC$d_ABC >= 0  | is.nan(list_ABC$d_ABC)))
+})
+
+
+test_that("compute_ABC_cpp works: optimized, with distances", {
+
+   n_summary <- expect_silent(get_number_summary_statistics(TRUE))
+   vec_eps <- rep(1, n_summary)
+
+   list_ABC <- expect_silent(compute_ABC_cpp(
+         n_sample = 10,
+         m_sample = 10,
+         alpha_0 = 1,
+         beta_0 = 1,
+         nu_0 = rep(1, p),
+         mtx_obs,
+         summarize_eps = vec_eps,
+         reps = 100,
+         p_norm = 2,
+         use_optimized_summary = TRUE,
+         return_distances = TRUE
+   ))
+
+   expect_is(list_ABC, 'list')
+   expect_named(list_ABC, c('n_accepted', 'accept_ratio', 'd_ABC'))
+   expect_gte(list_ABC$n_accepted, 0)
+   expect_lte(list_ABC$n_accepted, 100)
+   expect_gte(list_ABC$accept_ratio, 0)
+   expect_lte(list_ABC$accept_ratio, 1)
+   expect_is(list_ABC$d_ABC, 'matrix')
+   expect_equal(nrow(list_ABC$d_ABC), 100)
+   expect_equal(ncol(list_ABC$d_ABC), n_summary)
+   expect_true(all(list_ABC$d_ABC >= 0 | is.nan(list_ABC$d_ABC)))
+})
